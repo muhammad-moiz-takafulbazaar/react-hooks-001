dd89f71f30b40bd0a486402dda81986bbb9c10f8
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-24 12:48:34 +0500

    setState basic behavior explain

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,21 @@ import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+  /**
+   * increment function will add only one 1 in count state.
+   * why !? because it work synchronously and doesn't wait for line no 12 to update the state and then go further go every time we got state value as 0 ( if we click first time ).
+   */
+  const increment = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+    setCount(count + 1);
+  };
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         {count}
-        <button onClick={() => setCount(count + 1)}>
-          Add
-        </button>
+        <button onClick={increment}>Add</button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
