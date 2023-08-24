9a2bf29f602496d8fab0dba7118c3c74ffb941db
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-24 13:11:07 +0500

    useEffect explain infinite loop and return

--- src/components/useEffectExplain.tsx
+++ src/components/useEffectExplain.tsx
@@ -2,13 +2,24 @@ import React, { useEffect, useState } from 'react';
 
 const UseEffectExplain = () => {
   const [input, setInput] = useState('');
+  const [counter, setCounter] = useState(0);
   useEffect(() => {
-    document.title = input
-  }, [input])
+    // document.title = input
+    const increment = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(increment)
+    /**
+     * if you pass the value in dependency array which you are updating in the useEffect then it will crash your code and because you create a infinite loop 
+     */
+    // }, [counter]);
+  }, [input]);
   return (
     <div>
       <h1>{input}</h1>
       <h2>UseEffect</h2>
+      <h3>{counter}</h3>
       <input type='text' onChange={(e) => setInput(e.target.value)} />
     </div>
   );
