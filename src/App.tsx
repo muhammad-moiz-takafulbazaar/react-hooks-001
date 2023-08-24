48dfbe7f3c66de9a4ea5ddb1c29e62789e91de50
Mohammad Moiz Ali <muhammad.moiz@takafulbazaar.pk>
2023-08-24 12:49:54 +0500

    useState resolve last problem

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,14 @@ function App() {
   const [count, setCount] = useState(0);
   /**
    * increment function will add only one 1 in count state.
-   * why !? because it work synchronously and doesn't wait for line no 12 to update the state and then go further go every time we got state value as 0 ( if we click first time ).
+   * -- why !? because it work synchronously and doesn't wait for line no 12 to update the state and then go further go every time we got state value as 0 ( if we click first time ).
+   * 
+   * -- now we use a callback function inside the useState to it will get the latest value. 
    */
   const increment = () => {
-    setCount(count + 1);
-    setCount(count + 1);
-    setCount(count + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
+    setCount(prevCount => prevCount + 1);
   };
   return (
     <div className='App'>
